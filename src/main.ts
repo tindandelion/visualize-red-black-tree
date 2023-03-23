85aad6c80881ba4c3773d7dd8bfeed3b34c36001
Sergey Moshnikov <s.moshnikov@gmail.com>
2023-03-23 13:36:19 +0200

    Experimenting on rotations

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,88 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
+import P5 from 'p5'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+class NodePicture {
+  private static readonly RADIUS = 50
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  constructor(readonly x: number, readonly y: number, readonly value: any) {}
+
+  draw(canvas: P5) {
+    canvas.circle(this.x, this.y, NodePicture.RADIUS)
+    canvas.textAlign(canvas.CENTER, canvas.CENTER)
+    canvas.textSize(20)
+    canvas.text(this.value, this.x, this.y)
+  }
+
+  drawConnectionTo(other: NodePicture, canvas: P5) {
+    canvas.line(this.x, this.y, other.x, other.y)
+  }
+
+  isAtSamePositionAs(other: NodePicture) {
+    return this.x === other.x && this.y === other.y
+  }
+}
+
+const element = document.querySelector<HTMLDivElement>('#app')!
+
+const sketch = (p5: P5) => {
+  const duration = 1000
+
+  let root: NodePicture
+  let right: NodePicture
+  let left: NodePicture
+
+  p5.setup = () => {
+    p5.createCanvas(element.clientWidth, element.clientHeight)
+    root = new NodePicture(p5.width / 2, 100, 'E')
+    right = new NodePicture(root.x + 60, 200, 'S')
+    left = new NodePicture(root.x - 60, 200, '')
+  }
+
+  p5.draw = () => {
+    const timeElapsed = p5.millis()
+    if (timeElapsed > duration) return
+
+    const rootRotAngle = -(Math.PI / (2 * duration)) * timeElapsed
+    const rightRotAngle = rootRotAngle - (3 * Math.PI) / 2
+
+    const rotCenterX = root.x
+    const rotCenterY = right.y
+
+    const radiusX = right.x - root.x
+    const radiusY = right.y - root.y
+    const scale = radiusX / radiusY
+
+    const newRootX =
+      (Math.cos(rootRotAngle) * (root.x - rotCenterX) -
+        Math.sin(rootRotAngle) * (root.y - rotCenterY)) *
+        scale +
+      rotCenterX
+
+    const newRootY =
+      Math.sin(rootRotAngle) * (root.x - rotCenterX) +
+      Math.cos(rootRotAngle) * (root.y - rotCenterY) +
+      rotCenterY
+
+    const rootTemp = new NodePicture(newRootX, newRootY, root.value)
+    const newRightX =
+      (Math.cos(rightRotAngle) * (root.x - rotCenterX) -
+        Math.sin(rightRotAngle) * (root.y - rotCenterY)) *
+        scale +
+      rotCenterX
+
+    const newRightY =
+      Math.sin(rightRotAngle) * (root.x - rotCenterX) +
+      Math.cos(rightRotAngle) * (root.y - rotCenterY) +
+      rotCenterY
+    const rightTemp = new NodePicture(newRightX, newRightY, right.value)
+
+    p5.clear(255, 255, 255, 255)
+    p5.circle(rotCenterX, rotCenterY, 10)
+    left.draw(p5)
+    rightTemp.draw(p5)
+    rootTemp.draw(p5)
+    root.draw(p5)
+  }
+}
+
+new P5(sketch, element)
